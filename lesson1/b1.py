9124efd1fbe97fc5e5dfe430858958654457937e
unknown <artems.mikhaylets@gmail.com>
2021-12-01 15:10:06 +0200

    j

---
+++ lesson1/b1.py
@@ -0,0 +1,18 @@
+#a=int(input("a="))
+#b=int(input("b="))
+#print(a+b)
+#a=int(input("a="))
+#b=int(input("b="))
+#a+=b
+#a-=10
+#print(a)
+#sum = int(input('sum'))
+#p= int(input('%'))
+#for i in range(5):
+#    i =i+1
+#    sum = sum + sum*(p/100)
+#    print(int(sum))
+#a=int(input("высота"))
+#b=int(input("осн."))
+#s=(0.5 * a * b)
+#print("s=" , s)
---
+++ lesson2/lesson5.py
@@ -0,0 +1,94 @@
+#mashin= ['bmw' , 'suzu' , 'mazda' , 'lar' ]
+#print(mashin)
+#print (mashin[-1])
+
+#message = f"я  хочу 1  марку мвшины {mashin[0].title()}"
+#print(message)
+#print (mashin[0])
+#mashin[3]= 'ж'
+
+#mashin.append('fers')
+#print(mashin)
+#mashin.insert(0 , 'su')
+#num = int(input('sum'))
+#pr = int(input('какой процент'))
+#a = int(input('когда зб'))
+#if a > 12 :
+#    num2 = num +num * (pr/100)
+#    print(num2)
+#else:
+#     print(num)
+
+#a = int(input('какой по номер пр ?'))
+#les = ['ит' , 'мат' , 'гео' , 'инф' , 'физ']
+#if a == 1 :
+#    print(les[0])
+#elif a==2 :
+#    print(les[1])
+#elif a==3 :
+#    print(les[2])
+#elif a==4 :
+#    print(les[3])
+#elif a==5 :
+#    print(les[4])
+    
+#a = int(input('a='))
+#b = int(input('b='))
+#if a >b:
+#    print('a=1 b=0' )
+#elif a == b:
+#    print('a=b')
+#else:
+#    print('a=0 b=1' )
+
+#n = int(input('n='))
+#p =0.5
+#S=0
+#for i in range(1 , n+1):
+    
+#    S+=p
+#    p/=2
+    
+#    print(S)
+#print(S)
+
+#for i in range(1, 10,3):
+
+#    p=12*i
+#    print(i,p)
+
+#a = int(input('рост'))
+#if a > 180:
+#    print('man')
+#elif a<=180 and a>150 :
+#    print('gerl')
+#else:
+#    print('c')
+
+#num = [1,2,3,4,5]
+#s=0 
+#for i in num :
+#   s= s+i
+#print(s)
+
+#T = [1,2,3,4,5]
+#t = T[0]
+#for i in T :
+#    if t < i:
+#        t=i
+#print(i)
+
+#a = int(input('сторона'))
+#r = int(input('r'))
+#import math
+
+#b = (r * math.sqrt(3))/6
+#if b==a :
+#    print('yes')
+#else:
+#    print('no')
+
+#mashin = ['honda', 'lambo' , 'bmw' , 'mazda']
+
+#for a , item in enumerate (mashin):
+#    print (a +1 ,item)
